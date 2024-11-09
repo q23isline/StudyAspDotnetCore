26568aba42eee2e3dd619eab66a6e8fd7062b240
q23isline <rowpure@gmail.com>
2024-11-09 17:21:53 +0900

    add: レコードの更新日・作成日を登録する共通処理追加

---
+++ backend/Models/IRecordableTimestamp.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+namespace StudyAspDotnetCore.Models;
+public interface IRecordableTimestamp
+{
+    [Display(Name = "作成日")]
+    public DateTime CreatedAt { get; set; }
+
+    [Display(Name = "更新日")]
+    public DateTime LastUpdatedAt { get; set; }
+}
--- backend/Models/MyContext.cs
+++ backend/Models/MyContext.cs
@@ -3,4 +3,9 @@ namespace StudyAspDotnetCore.Models;
 public class MyContext : DbContext
 {
     public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.AddInterceptors(new TimestampInterceptor());
+    }
 }
