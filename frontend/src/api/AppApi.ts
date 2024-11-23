c74a55b84da868b8364a70e907fd973ddaae1f81
q23isline <rowpure@gmail.com>
2024-11-23 14:14:09 +0900

    add: 本番環境想定の設定を追加

--- frontend/src/api/AppApi.ts
+++ frontend/src/api/AppApi.ts
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'
+
 export const AppApi = axios.create({
-  // vite.config.ts で server.proxy を定義しても反映されないのでここで定義する
-  // TODO: ローカル用の設定になっているので、環境変数を読み込んで動的に設定できるようにする
-  baseURL: 'http://localhost:8080',
+  // vite.config.ts で server.proxy を定義しても反映されないのでここで URL 定義する
+  baseURL,
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
